fa640539f220b1b2c6bc34f4c4cf16f870f5d2b0
dbusk <writelani@gmail.com>
2017-09-27 16:58:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hella Insecure: Issa vs. Lawrence");
+    }
+}
+public class {
+
+    public static void main(String[] args) {
+
+        System.out.println("Insecure: Issa Hive vs. Lawrence Hive");
+
+        int teamIssa = 0;
+
+        int teamLawrence = 0;
+
+
+        System.out.println("Team Issa Score: " + team Issa);
+
+        System.out.println("Team Lawrence Score: " + teamLawrence);
+
+        Scanner keyboard = new Scanner(System.in);
+        }})
+
+        }
+
+        
